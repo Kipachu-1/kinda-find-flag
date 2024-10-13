75aa1ac4df17311d9061eafa55c1b1cc5bb486b9
Kipachu <arsik7473838@gmail.com>
2024-10-13 22:57:23 +0500

    Initial Commit

---
+++ kinda-find-flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kinda-find-flag
+//
+//  Created by Arsen Kipachu on 10/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kinda-find-flagTests/kinda_find_flagTests.swift
@@ -0,0 +1,17 @@
+//
+//  kinda_find_flagTests.swift
+//  kinda-find-flagTests
+//
+//  Created by Arsen Kipachu on 10/13/24.
+//
+
+import Testing
+@testable import kinda_find_flag
+
+struct kinda_find_flagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
